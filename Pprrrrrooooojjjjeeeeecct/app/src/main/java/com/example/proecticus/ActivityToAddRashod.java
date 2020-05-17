9b3f0364a9b1909401fe7c9b3516f7fcf0a6ef7b
nothegle <polieftovegor@gmail.com>
2020-05-17 16:06:30 +0500

    пофикси кнопку добавления расходов

--- Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/ActivityToAddRashod.java
+++ Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/ActivityToAddRashod.java
@@ -2,9 +2,17 @@ package com.example.proecticus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class ActivityToAddRashod extends AppCompatActivity {
+    EditText rashodToAdd;
+    TextView potratil, allMoney;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +20,18 @@ public class ActivityToAddRashod extends AppCompatActivity {
         setContentView(R.layout.activity_to_add_rashod);
     }
 
-    public void Click_AddBtn()
+    @SuppressLint("SetTextI18n")
+    public void Click_AddBtn(View v)
     {
-
+        rashodToAdd = findViewById(R.id.rashodi);
+        int rashod = Integer.parseInt(rashodToAdd.getText().toString());
+        potratil = findViewById(R.id.Potratil);
+        int allPotratil = Integer.parseInt(potratil.getText().toString());
+        allMoney = findViewById(R.id.allMoney);
+        int allMon = Integer.parseInt(allMoney.getText().toString());
+        int newAllPotratil = allPotratil + rashod;
+        potratil.setText(Integer.toString(newAllPotratil));
+        int newAllMoney = allMon - rashod;
+        allMoney.setText(Integer.toString(newAllMoney));
     }
 }
