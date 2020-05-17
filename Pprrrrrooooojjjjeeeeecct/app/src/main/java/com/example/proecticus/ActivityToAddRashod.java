e3e75c3b4c0a09f4d01a24f5c8faf17198ded2a8
nothegle <polieftovegor@gmail.com>
2020-05-17 15:01:33 +0500

    страница с добавлением расхода 1

---
+++ Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/ActivityToAddRashod.java
@@ -0,0 +1,19 @@
+package com.example.proecticus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityToAddRashod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_add_rashod);
+    }
+
+    public void Click_AddBtn()
+    {
+
+    }
+}
--- Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/MainActivity.java
+++ Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.proecticus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Click_Products(View v)
+    {
+        Intent intent = new Intent(this, ActivityToAddRashod.class);
+        startActivity(intent);
+    }
+
 }
