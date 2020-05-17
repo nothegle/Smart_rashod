b03f1ea25808239b2276a21227434f0c48b18b5a
nothegle <polieftovegor@gmail.com>
2020-05-17 12:41:23 +0500

    переделал интерфейс

--- Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/MainActivity.java
+++ Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.proecticus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
