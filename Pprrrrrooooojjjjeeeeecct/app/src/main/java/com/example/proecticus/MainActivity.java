3eaa34d6f1acb0e6376fe68b638df665b3529847
nothegle <polieftovegor@gmail.com>
2020-05-18 17:08:14 +0500

    сохранение данных allMoney и allExpenses

--- Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/MainActivity.java
+++ Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.proecticus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,10 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
     private Button products;
     public TextView Date, allMoney, allExpenses, productsExpenses;
+    SharedPreferences sPref;
+    final String ALLMONEY = "ALLMONEY";
+    final String  ALLEXPENCSES = "ALLEXPENCSES";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         allExpenses = findViewById(R.id.allExpenses);
         products = findViewById(R.id.products);
         productsExpenses = findViewById(R.id.productsExpenses);
+        LoadData();
     }
 
     public void Click_Products(View v)
@@ -56,5 +62,27 @@ public class MainActivity extends AppCompatActivity {
         allMoney.setText(Integer.toString(newAllMon));
         allExpenses.setText(Integer.toString(newAllExp));
 
+    }
+
+    private void SaveData(){
+        sPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putString(ALLMONEY, allMoney.getText().toString());
+        ed.putString(ALLEXPENCSES, allExpenses.getText().toString());
+        ed.commit();
+    }
+
+    private void LoadData(){
+        sPref = getPreferences(MODE_PRIVATE);
+        String savedAllMoney = sPref.getString(ALLMONEY, "0");
+        String savedAllExpenses = sPref.getString(ALLEXPENCSES, "0");
+        allMoney.setText(savedAllMoney);
+        allExpenses.setText(savedAllExpenses);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SaveData();
     }
 }
