ccdf47bb7ce7a8688b6703e4285198957a4b173f
nothegle <polieftovegor@gmail.com>
2020-05-18 13:29:52 +0500

    сделал добавление расходов для продуктов

--- Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/ActivityToAddRashod.java
+++ Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/ActivityToAddRashod.java
@@ -3,6 +3,7 @@ package com.example.proecticus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
@@ -13,28 +14,31 @@ import android.widget.TextView;
 
 public class ActivityToAddRashod extends AppCompatActivity {
     EditText rashodToAdd;
-    TextView potratil, allMoney;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_add_rashod);
+        rashodToAdd = findViewById(R.id.rashodi);
     }
 
     @SuppressLint("SetTextI18n")
     public void Click_AddBtn(View v)
     {
-        rashodToAdd = findViewById(R.id.rashodi);
-        
-        View view = getLayoutInflater().inflate(R.layout.activity_main, null);
-        TextView potratil = view.findViewById(R.id.Potratil);
-        TextView allMoney = view.findViewById(R.id.allMoney);
-        int rashod = Integer.parseInt(rashodToAdd.getText().toString());
-        int allPotratil = Integer.parseInt(potratil.getText().toString());
-        int allMon = Integer.parseInt(allMoney.getText().toString());
-        int newAllPotratil = allPotratil + rashod;
-        int newAllMoney = allMon - rashod;
-        potratil.setText(Integer.toString(newAllPotratil));
-        allMoney.setText(Integer.toString(newAllMoney));
+        Intent newExpense = new Intent();
+        newExpense.putExtra("rashod", rashodToAdd.getText().toString());
+        setResult(RESULT_OK, newExpense);
+        finish();
+
+        //View view = getLayoutInflater().inflate(R.layout.activity_main, null);
+        //TextView potratil = view.findViewById(R.id.Potratil);
+        //TextView allMoney = view.findViewById(R.id.allMoney);
+
+        //int allPotratil = Integer.parseInt(potratil.getText().toString());
+        //int allMon = Integer.parseInt(allMoney.getText().toString());
+        //int newAllPotratil = allPotratil + rashod;
+        //int newAllMoney = allMon - rashod;
+       // potratil.setText(Integer.toString(newAllPotratil));
+       // allMoney.setText(Integer.toString(newAllMoney));
     }
 }
--- Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/MainActivity.java
+++ Pprrrrrooooojjjjeeeeecct/app/src/main/java/com/example/proecticus/MainActivity.java
@@ -9,19 +9,49 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     private Button products;
-
+    public TextView Date, allMoney, allExpenses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Date = findViewById(R.id.Date);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd:MM:yy");
+        String time = sdf.format(new Date(System.currentTimeMillis()));
+        Date.setText(time);
+        allMoney = findViewById(R.id.allMoney);
+        allExpenses = findViewById(R.id.allExpenses);
+        products = findViewById(R.id.products);
+
     }
 
     public void Click_Products(View v)
     {
         Intent intent = new Intent(this, ActivityToAddRashod.class);
-        startActivity(intent);
+        startActivityForResult(intent, 1);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data == null) {
+            return;
+        }
+        String newExpense = data.getStringExtra("rashod");
+        int newExp = 0;
+        if(newExpense != "")
+            newExp = Integer.parseInt(newExpense);
+        int allMon = Integer.parseInt(allMoney.getText().toString());
+        int allExp = Integer.parseInt(allExpenses.getText().toString());
+        int newAllMon = allMon - newExp;
+        int newAllExp = allExp + newExp;
+        allMoney.setText(Integer.toString(newAllMon));
+        allExpenses.setText(Integer.toString(newAllExp));
+
+    }
 }
